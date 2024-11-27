f8ee26747845561442b0d0d97851eb2ad07af65a
Your Name <k.bristi057@gmail.com>
2024-11-28 00:51:00 +0600

    added the get and delete method with post

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000 ;
 const app = express();
  
 // 22103412
 // omDSUul9PY4pvChT
 
-
+app.use(cors())
+app.use(express.json());
 
 const uri = "mongodb+srv://22103412:omDSUul9PY4pvChT@cluster0.i7pwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
@@ -24,9 +25,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const database = client.db("usersDB");
-    const usersCollection = database.collection("users");
+    // const database = client.db("usersDB");
+    const usersCollection = client.db("usersDB").collection("users");
+// get or read
+app.get('/users', async(req,res)=>{
+  const cursor = usersCollection.find();
+  const result = await cursor.toArray()
+  res.send(result);
 
+})
+
+app.get(`/users/:id`, async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id:new ObjectId(id)}
+  const user = await usersCollection.findOne(query);
+  res.send(user);
+})
+
+app.delete('/users/:id', async(req,res)=>{
+  const id = req.params.id;
+  console.log('Please delete from database', id);
+  const query= {_id:new ObjectId(id)};
+  const result = await usersCollection.deleteOne(query)
+})
+
+// post or create
     app.post('/users', async(req,res)=>{
         const user = req.body;
         console.log('New user:',user);
@@ -43,15 +66,14 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
-app.use(cors())
-app.use(express.json());
+
 
 app.get('/', (req,res)=>{
     res.send('Simple crud is running')
